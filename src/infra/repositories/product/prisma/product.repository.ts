37cd32aa25cd2ab6a7fa16f7cde9a63282983415
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-06-28 21:52:12 -0300

    create repository & api

---
+++ src/infra/repositories/product/prisma/product.repository.ts
@@ -0,0 +1,40 @@
+import type { PrismaClient } from '@prisma/client'
+
+import { Product } from '../../../../domain/product/entity/product.entity'
+import type { ProductGateway } from '../../../../domain/product/gateway/product.gateway'
+
+export class ProductRepositoryPrisma implements ProductGateway {
+  private constructor(private readonly prismaClient: PrismaClient) {}
+
+  public static create(prismaClient: PrismaClient) {
+    return new ProductRepositoryPrisma(prismaClient)
+  }
+
+  public async save(product: Product): Promise<void> {
+    const data = {
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      quantity: product.quantity,
+    }
+
+    await this.prismaClient.product.create({
+      data,
+    })
+  }
+
+  public async list(): Promise<Product[]> {
+    const products = await this.prismaClient.product.findMany()
+
+    const productList = products.map((product) => {
+      return Product.with({
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        quantity: product.quantity,
+      })
+    })
+
+    return productList
+  }
+}
